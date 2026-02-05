0d1f208fc50986fd6a19faa2bc1572406a4c0978
kallewatsch <elnerdo@web.de>
2026-02-05 09:37:13 +0100

    Search working now

--- src/app/components/Search.js
+++ src/app/components/Search.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { useDispatch } from 'react-redux'
 import { useLazySearchAnythingQuery } from "../api"
 import { setData } from "../appSlice"
@@ -6,17 +6,22 @@ import { setData } from "../appSlice"
 export const Search = () => {
 
     const [search] = useLazySearchAnythingQuery()
+    const [searchText, setSearchText] = useState('')
 
     const dispatch = useDispatch()
 
+    const handleChange = event => {
+        setSearchText(event.target.value)
+    }
+
     const handleSearch = event => {
-        dispatch(setData("soon"))
-        /* getBattles().then(result => {
+        search({searchText}).then(result => {
             dispatch(setData(result.data.result.data))
-        }) */
+        })
     }
 
     return <>
+        <input type="text" onChange={handleChange} />
         <button onClick={handleSearch}>search</button>
     </>
 }
