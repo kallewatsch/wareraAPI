200be3d6afbaf0ece1a91444a8325e5bda64ed3f
kallewatsch <elnerdo@web.de>
2026-02-05 09:32:54 +0100

    adds simple Navigation

---
+++ src/app/components/Navigation.js
@@ -0,0 +1,36 @@
+import React from "react"
+import Nav from "react-bootstrap/Nav"
+
+const routes = [
+    "search",
+    "companies",
+    "events",
+    "governments",
+    "regions",
+    "battles",
+    "rounds",
+    "battlerankings",
+    "prices",
+    "tradingorders",
+    "itemoffers",
+    "workoffers",
+    "rankings",
+    "gameconfig",
+    "users",
+    "articles",
+    "mus",
+    "transactions",
+    "upgrade",
+    "workers"
+]
+
+export const Navigation = () => {
+    return (
+        <Nav>
+            <Nav.Item><Nav.Link href="/">freemus</Nav.Link></Nav.Item>
+            {routes.map((route, i) => <Nav.Item key={`route-${i}`}><Nav.Link href={`/#${route}`}>{route}</Nav.Link></Nav.Item>)}
+        </Nav>
+    )
+}
+
+export default Navigation
