7001771a31df19570907676c90593bdf243da668
kallewatsch <elnerdo@web.de>
2026-02-10 05:55:49 +0100

    appSlice tests

--- src/app/appSlice.test.js
+++ src/app/appSlice.test.js
@@ -8,7 +8,9 @@ import reducer, {
     setUsers,
     addUsers,
     setRegions,
-    setCompanies
+    setCompanies,
+    setWarPlaner,
+    resetWarPlaner
 } from "./appSlice"
 
 describe("appSlice", () => {
@@ -21,14 +23,24 @@ describe("appSlice", () => {
             isLoading: false,
             users: [],
             regions: [],
-            companies: []
+            companies: [],
+            warplaner: {
+                attackers: {
+                    ids: [],
+                    countries: []
+                },
+                defenders: {
+                    ids: [],
+                    countries: []
+                }
+            }
         })
     })
     it("should return initialState", () => {
         expect(reducer(undefined, {})).toEqual(initialState)
     })
     it("should handle setData", () => {
-        let foo = {bar: 'baz'}
+        let foo = { bar: 'baz' }
         expect(reducer(initialState, setData(foo))).toEqual({
             ...initialState,
             data: foo
@@ -42,10 +54,73 @@ describe("appSlice", () => {
         })
     })
     it("should handle setCountries", () => {
-        const data = [{foo: 'bar'}, {abc: '123'}]
+        const data = [{ foo: 'bar' }, { abc: '123' }]
         expect(reducer(initialState, setCountries(data))).toEqual({
             ...initialState,
             countries: data
+        })
+    })
+    it("should handle addMus", () => {
+        const data = [{ foo: 'bar' }, { abc: '123' }]
+        expect(reducer(initialState, addMus(data))).toEqual({
+            ...initialState,
+            mus: data
+        })
+    })
+    it("should handle setIsLoading", () => {
+        expect(reducer(initialState, setIsLoading(true))).toEqual({
+            ...initialState,
+            isLoading: true
+        })
+    })
+    it("should handle setUsers", () => {
+        const data = [{ foo: 'bar' }, { abc: '123' }]
+        expect(reducer(initialState, setUsers(data))).toEqual({
+            ...initialState,
+            users: data
+        })
+    })
+    it("should handle addUsers", () => {
+        const existingUsers = [{ foo: 'bar' }, { abc: '123' }]
+        const data = [{ bla: 'bla' }]
+        const state = Object.assign({}, { ...initialState }, { users: existingUsers })
+        expect(reducer(state, addUsers(data))).toEqual({
+            ...initialState,
+            users: [...existingUsers, ...data]
+        })
+    })
+    it("should handle setRegions", () => {
+        const data = [{ foo: 'bar' }, { abc: '123' }]
+        expect(reducer(initialState, setRegions(data))).toEqual({
+            ...initialState,
+            regions: data
+        })
+    })
+    it("should handle setCompanies", () => {
+        const data = [{ foo: 'bar' }, { abc: '123' }]
+        expect(reducer(initialState, setCompanies(data))).toEqual({
+            ...initialState,
+            companies: data
+        })
+    })
+    it("should handle setWarPlaner", () => {
+        const data = {
+            attackers: { ids: ['1', '42'], countires: [{ foo: 'bar' }, { abc: '123' }] },
+            defenders: { ids: ['69'], countries: [{bla: 'bla'}]}
+        }
+        expect(reducer(initialState, setWarPlaner(data))).toEqual({
+            ...initialState,
+            warplaner: data
+        })
+    })
+    it("should handle resetWarPlaner", () => {
+        const data = {
+            attackers: { ids: ['1', '42'], countires: [{ foo: 'bar' }, { abc: '123' }] },
+            defenders: { ids: ['69'], countries: [{bla: 'bla'}]}
+        }
+        const state = Object.assign({}, {...initialState}, {warplaner: data})
+        expect(reducer(state, resetWarPlaner())).toEqual({
+            ...initialState
         })
     })
 })
