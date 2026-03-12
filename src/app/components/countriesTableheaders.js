622c6204a7a52373a2cca42a2a089a63828006fb
kallewatsch <elnerdo@web.de>
2026-03-12 14:13:17 +0100

    adds icons for countries sortable table

--- src/app/components/countriesTableheaders.js
+++ src/app/components/countriesTableheaders.js
@@ -1,24 +1,40 @@
-import { GiAxeSword, GiCharacter, GiPeaceDove, GiShield } from "react-icons/gi"
+import {
+    GiAqueduct,
+    GiAxeSword,
+    GiClawHammer,
+    GiCoinsPile,
+    GiDeadlyStrike,
+    GiDeathStar,
+    GiFlamethrowerSoldier,
+    GiFlyingFlag,
+    GiMountains,
+    GiOpenTreasureChest,
+    GiPeaceDove,
+    GiShield,
+    GiSpikyExplosion,
+    GiStarFormation,
+    GiTeamUpgrade
+} from "react-icons/gi"
 import { createIconWithOverlayTh } from "./util/table/thCreators"
 
+const thCountryName = createIconWithOverlayTh({ icon: { component: GiFlyingFlag }, title: "Country Name", txt: "Name", attrPath: "", target: "name" })
+
 const thsFoo = [
-    { icon: { component: GiCharacter }, title: "Country Name", txt: "Name", attrPath: "", target: "name" },
-    { icon: { component: GiCharacter }, title: "Country Region Diff", txt: "Region Diff", attrPath: ["rankings", "countryRegionDiff"], target: "value" },
-    { icon: { component: GiCharacter }, title: "Country Damages", txt: "Damages", attrPath: ["rankings", "countryDamages"], target: "value" },
-    { icon: { component: GiCharacter }, title: "Country Weekly Damages", txt: "Weekly Damages", attrPath: ["rankings", "weeklyCountryDamages"], target: "value" },
-    { icon: { component: GiCharacter }, title: "Country Damage per Citizen", txt: "Damage per Citizen", attrPath: ["rankings", "weeklyCountryDamagesPerCitizen"], target: "value" },
-    { icon: { component: GiCharacter }, title: "Country Development", txt: "Development (rankings)", attrPath: ["rankings", "countryDevelopment"], target: "value" },
-    { icon: { component: GiCharacter }, title: "Country Active Population", txt: "Active Population", attrPath: ["rankings", "countryActivePopulation"], target: "value" },
-    { icon: { component: GiCharacter }, title: "Country Wealth", txt: "Wealth", attrPath: ["rankings", "countryWealth"], target: "value" },
-    { icon: { component: GiCharacter }, title: "Country Bounty", txt: "Bounty", attrPath: ["rankings", "countryBounty"], target: "value" },
-    { icon: { component: GiCharacter }, title: "Country Production Bonus", txt: "Production Bonus", attrPath: ["rankings", "countryProductionBonus"], target: "value" },
-    { icon: { component: GiCharacter }, title: "Country Development", txt: "Development", attrPath: "", target: "development" },
-    { icon: { component: GiCharacter }, title: "Country Specialized Item", txt: "Specialized Item", attrPath: "", target: "specializedItem" },
+    { icon: { component: GiMountains }, title: "Country Region Diff", txt: "Region Diff", attrPath: ["rankings", "countryRegionDiff"], target: "value" },
+    { icon: { component: GiDeathStar }, title: "Country Damages", txt: "Damages", attrPath: ["rankings", "countryDamages"], target: "value" },
+    { icon: { component: GiSpikyExplosion }, title: "Country Weekly Damages", txt: "Weekly Damages", attrPath: ["rankings", "weeklyCountryDamages"], target: "value" },
+    { icon: { component: GiFlamethrowerSoldier }, title: "Country Damage per Citizen", txt: "Damage per Citizen", attrPath: ["rankings", "weeklyCountryDamagesPerCitizen"], target: "value" },
+    { icon: { component: GiAqueduct }, title: "Country Development", txt: "Development (rankings)", attrPath: ["rankings", "countryDevelopment"], target: "value" },
+    { icon: { component: GiTeamUpgrade }, title: "Country Active Population", txt: "Active Population", attrPath: ["rankings", "countryActivePopulation"], target: "value" },
+    { icon: { component: GiCoinsPile }, title: "Country Wealth", txt: "Wealth", attrPath: ["rankings", "countryWealth"], target: "value" },
+    { icon: { component: GiOpenTreasureChest }, title: "Country Bounty", txt: "Bounty", attrPath: ["rankings", "countryBounty"], target: "value" },
+    { icon: { component: GiClawHammer }, title: "Country Production Bonus", txt: "Production Bonus", attrPath: ["rankings", "countryProductionBonus"], target: "value" },
+    { icon: { component: GiAqueduct }, title: "Country Development", txt: "Development", attrPath: "", target: "development" },
+    { icon: { component: GiStarFormation }, title: "Country Specialized Item", txt: "Specialized Item", attrPath: "", target: "specializedItem" },
 ]
 
 const thsBla = [
-    { icon: { component: GiCharacter }, title: "Country Name", txt: "Name", attrPath: "", target: "name" },
-    { icon: { component: GiCharacter }, title: "Country Total Available Damage", txt: "Available Damage (No Food)", attrPath: ["extended"], target: "totalAvailableCountryDmg" },
+    { icon: { component: GiDeadlyStrike }, title: "Country Total Available Damage", txt: "Available Damage (No Food)", attrPath: ["extended"], target: "totalAvailableCountryDmg" },
 ]
 
 const thsUpgrades = [
@@ -32,7 +48,7 @@ const thsUpgrades = [
 }).flat()
 
 export const countriesTabs = [
-    { name: "Country Region Upgrades", ths: [...thsUpgrades.map(th => createIconWithOverlayTh(th))] },
-    { name: "Basic", ths: [...thsFoo.map(th => createIconWithOverlayTh(th))] },
-    { name: "Extended", ths: [...thsBla.map(th => createIconWithOverlayTh(th))] }
+    { name: "Country Region Upgrades", ths: [thCountryName, ...thsUpgrades.map(th => createIconWithOverlayTh(th))] },
+    { name: "Basic", ths: [thCountryName, ...thsFoo.map(th => createIconWithOverlayTh(th))] },
+    { name: "Extended", ths: [thCountryName, ...thsBla.map(th => createIconWithOverlayTh(th))] }
 ]
