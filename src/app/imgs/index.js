df0923f320f3cbf0e0fbc6959d05b6c0aaf44096
kallewatsch <elnerdo@web.de>
2026-03-17 13:27:11 +0100

    moves images

---
+++ src/app/imgs/index.js
@@ -0,0 +1,31 @@
+import imgBoots from "./boots.png"
+import imgCase1 from "./case1.png"
+import imgChest from "./chest.png"
+import imgGloves from "./gloves.png"
+import imgGun from "./gun.png"
+import imgHelmet from "./helmet.png"
+import imgJet from "./jet.png"
+import imgKnife from "./knife.png"
+import imgLightAmmo from "./lightAmmo.png"
+import imgPants from "./pants.png"
+import imgRifle from "./rifle.png"
+import imgSniper from "./sniper.png"
+import imgTank from "./tank.png"
+
+export const imgs = {
+    imgBoots,
+    imgCase1,
+    imgChest,
+    imgGloves,
+    imgGun,
+    imgHelmet,
+    imgJet,
+    imgKnife,
+    imgLightAmmo,
+    imgPants,
+    imgRifle,
+    imgSniper,
+    imgTank
+}
+
+export default imgs
